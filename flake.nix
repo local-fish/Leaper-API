9f556a8d3624eb4cb483ac364f23a6dbfa42db0d
local-fish <Andrew.arinardi@gmail.com>
2026-05-07 14:16:33 +0700

    Build: Added Flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+	};
+
+	outputs = { self, nixpkgs }@inputs:
+	let
+		system = "x86_64-linux";
+		
+		pkgs = import inputs.nixpkgs{
+			inherit system;
+			config.allowUnfree = true;
+		};
+	in 
+	{
+		devShells.${system}.default = pkgs.mkShell rec {
+			name="Leaper-API";
+			packages = with pkgs; [
+				nodejs
+				];
+			shellHook = "tmux -L Leaper-API new-session -A -s Leaper-API";
+			};
+		};
+	}
