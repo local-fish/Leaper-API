fe616b92fbbc5030f8d1fc40ce54b1b5d7b32593
local-fish <Andrew.arinardi@gmail.com>
2026-05-12 19:59:40 +0700

    Fix: Wrapped Main.ts in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,30 @@ import { ValidationPipe } from '@nestjs/common';
 import App from '#common/app';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-if (process.env.DEV) console.warn('Running in development mode')
+if (process.env.DEV === 'true') console.warn('Running in development mode')
 
-const app = await NestFactory.create(App);
-app.useGlobalPipes(new ValidationPipe({
-	whitelist: true,
-	forbidNonWhitelisted: true,
-	transform: true
-}))
+async function bootstrap() {
+  const app = await NestFactory.create(App);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }))
 
-if (process.env.DEV) {
-	const swaggerDoc = new DocumentBuilder()
-		.setTitle('Leaper')
-		.setVersion('1.0.0')
-		.addBearerAuth()
-		.build()
+  if (process.env.DEV === 'true') {
+    const swaggerDoc = new DocumentBuilder()
+    .setTitle('Leaper')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build()
 
-	SwaggerModule.setup('swagger', app, () => SwaggerModule.createDocument(app, swaggerDoc))
-	console.log('Swagger applied')
+    SwaggerModule.setup('swagger', app, () => SwaggerModule.createDocument(app, swaggerDoc))
+    console.log('Swagger applied')
+  }
+
+
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 
-await app.listen(process.env.PORT ?? 3000);
-console.log('Started on port', process.env.PORT ?? 3000)
+bootstrap();
