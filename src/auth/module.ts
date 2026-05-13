40fa3c7a8f73e0d33bcd36468111042dc93ae445
frostice482 <frostice48260@gmail.com>
2026-05-13 22:09:00 +0700

    move assertions

--- src/auth/module.ts
+++ src/auth/module.ts
@@ -5,9 +5,6 @@ import assert from 'assert'
 import UserModule from "../user/module";
 import AppJwt from "../common/jwt";
 
-assert(process.env.PASSWORD_SECRET, 'PASSWORD_SECRET not defined')
-assert(process.env.JWT_SECRET, 'JWT_SECRET not defined')
-
 @Module({
 	imports: [UserModule, AppJwt],
 	controllers: [AuthController],
