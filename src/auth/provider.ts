0f0c495ad875d0d733a0896282937141054018d5
local-fish <Andrew.arinardi@gmail.com>
2026-06-06 23:34:40 +0700

    Fix: Agnosticism in the password hash too
    Oops!

--- src/auth/provider.ts
+++ src/auth/provider.ts
@@ -21,7 +21,7 @@ export default class AuthProvider {
 	}
 
 	async getIdentifier(email: string, password: string) {
-		const hashedpw = this.hashPassword(email, password)
+		const hashedpw = this.hashPassword(email.toLowerCase(), password)
 		const res = await db.user.findFirst({
 			select: { id: true },
 			where: { email: email.toLowerCase(), pwhash: hashedpw }
