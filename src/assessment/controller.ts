fd37892b2261631cd4342c7db524b9c6c9b792ba
local-fish <Andrew.arinardi@gmail.com>
2026-06-29 21:11:59 +0700

    Refactor: Removed Assessment Module

--- src/assessment/controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from "@nestjs/common";
-
-@Controller()
-export default class AssessmentController {}
--- src/assessment/provider.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@nestjs/common";
-
-@Injectable()
-class AssessmentProvider {}
-
-namespace AssessmentProvider {}
-
-export default AssessmentProvider
